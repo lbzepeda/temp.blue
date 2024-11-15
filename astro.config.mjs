623fae7c0e487a1dbb7ae4bcd32a269bde691944
lbzepeda <levi.zepeda06@gmail.com>
2024-11-15 14:46:31 -0600

    build(build): build into production

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+  },
   integrations: [
     tailwind({
       config: { path: './tailwind.config.mjs' },
