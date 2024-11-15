f617dc4920cc36db2b3b344424a4a5b149501553
lbzepeda <levi.zepeda06@gmail.com>
2024-11-15 16:05:21 -0600

    style(about): changue style in about section, now looks like blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  server: {
-    host: true,
-  },
   integrations: [
     tailwind({
       config: { path: './tailwind.config.mjs' },
