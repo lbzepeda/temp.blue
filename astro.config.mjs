77c945082605fb7ffd06b73c4791bef97b7138f7
lbzepeda <levi.zepeda06@gmail.com>
2024-11-18 21:17:37 -0600

    feat(seo): small imprivement SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://www.tempbluenicaragua.com',
   output: 'static',
   server: {
     host: true,
   },
   integrations: [
+    sitemap(),
     tailwind({
       config: { path: './tailwind.config.mjs' },
       applyBaseStyles: true,
